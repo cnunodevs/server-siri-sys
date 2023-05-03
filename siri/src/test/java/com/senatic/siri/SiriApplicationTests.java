5927c3b05442a0503c4b30e270fc24a4d4137463
JordanDev <100624493+JordanDev18@users.noreply.github.com>
2023-05-03 15:16:05 -0500

    default project added

---
+++ siri/src/test/java/com/senatic/siri/SiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senatic.siri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
