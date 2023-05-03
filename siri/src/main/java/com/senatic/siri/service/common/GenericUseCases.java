56a9154b67a89eff7eefc1cadf0b08be7ae03dd6
JordanDev <eduardoramirezdev@outlook.com>
2023-05-03 17:00:06 -0500

    Update GenericUseCases.java

--- siri/src/main/java/com/senatic/siri/service/common/GenericUseCases.java
+++ siri/src/main/java/com/senatic/siri/service/common/GenericUseCases.java
@@ -7,11 +7,12 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-public interface GenericUseCases <T> {
+public interface GenericUseCases <T, U> {
 
     List<T> handleListAll();
     void handleDelete(T t);
     void handleUpdate(T t);
+    void handleDeleteById(U u);
     Optional<T> handleFindOneByExample(Example<T> example, Pageable pageable);
     Optional<T> handleFindAllByExample(Example<T> example);
     Page<T> handleFindAllPaginate(Pageable pageable);
