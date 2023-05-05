7bd282ce751af32086dcdb9fc2dfcdde491bc438
Eduardo Ramirez <eduardoramirezdev@outlook.com>
2023-05-05 13:44:03 -0500

    Adding dtos ids

--- siri/src/main/java/com/senatic/siri/model/dto/VoluntariosInstructoresFormadosColDTO.java
+++ siri/src/main/java/com/senatic/siri/model/dto/VoluntariosInstructoresFormadosColDTO.java
@@ -1,5 +1,14 @@
 package com.senatic.siri.model.dto;
 
+import java.util.UUID;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class VoluntariosInstructoresFormadosColDTO {
+
+    private UUID id;
     
 }
