af8cd1d8993091a839fcdd8d1b8658b2ccc2aebe
edev404 <eramirez@extreme.com.co>
2023-06-25 13:41:04 -0500

    Update ApiExceptionHandler.java

--- siri/src/main/java/com/senatic/siri/exception/ApiExceptionHandler.java
+++ siri/src/main/java/com/senatic/siri/exception/ApiExceptionHandler.java
@@ -1,9 +1,22 @@
 package com.senatic.siri.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class ApiExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(ApiExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    public void genericExceptionHandler(Exception exception){
+        logger.error(exception.getMessage());
+    }
 
     
 }
